0a5aa6771c42c881609050da4b8e40ce41c341f8
fangkunhao <fangkunhao@agora.io>
2021-07-22 16:58:42 +0800

    cut the elf size by remove debug info

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 if [ ! -d "build/" ];then
   mkdir build
 fi
-g++ -Wall -W -Os -g -Ilinenoise -Iinclude src/main.cpp src/debugger.cpp src/breakpoint.cpp src/ptrace_wrapper.cpp lib/third_party/liblinenoise.a -o build/fdb
+g++ -Wall -W -Os -Ilinenoise -Iinclude src/main.cpp src/debugger.cpp src/breakpoint.cpp src/ptrace_wrapper.cpp lib/third_party/liblinenoise.a -o build/fdb
 g++ -g demos/demo.cpp -o build/demo
